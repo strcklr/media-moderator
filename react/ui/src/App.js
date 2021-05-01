551fd78d664d5749cb6ba81f62218094f40b4bb1
Chase Strickler <strickler.16@gmail.com>
2021-05-01 16:10:59 -0500

    Add ability to click anywhere in frame to open file chooser

--- react/ui/src/App.js
+++ react/ui/src/App.js
@@ -1,64 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useState } from 'react';
 import { HiUpload } from 'react-icons/hi';
 import { TiDelete } from 'react-icons/ti';
 import './App.css';
 import { FileDrop } from 'react-file-drop';
 
-class App extends Component {
-  state = {
-    haveFile: false,
-    uploadText: "Drag and drop a file to be scanned. Click to browse.",
-    file: "unkkj;lkjasdl;kfja;lsdkfja;lsdkjown.mp4",
+function App () {
+  const fileInputRef = useRef(null);
+  const onTargetClick = () => {
+    if (!haveFile) {
+      fileInputRef.current.click();
+    }
+  }
+  const [haveFile, setHaveFile] = useState(false);
+  const [uploadText, setUploadText] =  useState("Drag and drop a file to be scanned. Click to browse.");
+  const [file, setFile] = useState(null);
+  const [filename, setFileName] = useState("unknown.mp4");
+
+  var handleFileUpload = (files) => {
+    setFile(files[0]);
+    setFileName(files[0].name);
+    setUploadText("Ready to process file: " + files[0].name);
+    setHaveFile(true);
   }
 
-  removeFile = () => {
-    this.setState({
-      haveFile: false,
-      uploadText: "Drag and drop a file to be scanned. Click to browse.",
-      file: "unknown.mp4"
-    });
+  var removeFile = () => {
+    setHaveFile(false);
+    setFile(null);
+    setFileName("unknown.mp4");
+    setUploadText("Drag and drop a file to be scanned. Click to browse.");
   }
 
-  getUploadIcon() {
-    if (!this.state.haveFile) {
+  var onFileInputChange = (event) => {
+    const { files } = event.target;
+    handleFileUpload(files);
+  }
+
+  function getUploadIcon() {
+    if (!haveFile) {
       return <HiUpload className='upload'/>
     } else {
-      return <button className="removeButton" onClick={this.removeFile}><span className="filename">{this.state.file}</span> <TiDelete id="deleteIcon"/></button>
+      return <button className="removeButton" onClick={removeFile}><span className="filename">{filename}</span> <TiDelete id="deleteIcon"/></button>
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="banner">
-          <h1 id='title'>Media Moderator</h1>
-          <h4 id='subtitle'>View content with confidence</h4>
-          <div className="fileDrop">
-            <FileDrop
-              onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
-              onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
-              onFrameDrop={(event) => console.log('onFrameDrop', event)}
-              onDragOver={(event) => console.log('onDragOver', event)}
-              onDragLeave={(event) => console.log('onDragLeave', event)}
-              onDrop={(files, event) => {
-                console.log('onDrop!', files, event);
-                console.log(files);
-                const filename = files[0].name;
-                this.setState({
-                  file: filename,
-                  haveFile: true,
-                  uploadText: "Ready to process file: " + filename
-                })
-
-              }}>
-              {this.getUploadIcon()}
-              <p id="fileDropInstruction">{this.state.uploadText}</p>
-            </FileDrop>
-          </div>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="banner">
+        <h1 id='title'>Media Moderator</h1>
+        <h4 id='subtitle'>View content with confidence</h4>
+        <div className="fileDrop">
+          <FileDrop
+            onTargetClick={onTargetClick}
+            onFrameDragEnter={(event) => console.log('onFrameDragEnter', event)}
+            onFrameDragLeave={(event) => console.log('onFrameDragLeave', event)}
+            onFrameDrop={(event) => console.log('onFrameDrop', event)}
+            onDragOver={(event) => console.log('onDragOver', event)}
+            onDragLeave={(event) => console.log('onDragLeave', event)}
+            onDrop={(files, event) => {
+              console.log('onDrop!', files, event);
+              console.log(files);
+              handleFileUpload(files);
+            }}>
+            {getUploadIcon()}
+            <p id="fileDropInstruction">{uploadText}</p>
+            <input
+              onChange={onFileInputChange}
+              ref={fileInputRef}
+              type="file"
+              className="hidden"
+            />
+          </FileDrop>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 
