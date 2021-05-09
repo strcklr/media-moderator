cd7758d37cb6c1722f660f21c38d1435f81b52ba
Chase Strickler <strickler.16@gmail.com>
2021-05-08 21:40:51 -0500

    Refactor into packages, begin adding progress indicators for upload and processing

--- react/ui/src/screens/Home.js
+++ react/ui/src/screens/Home.js
@@ -1,11 +1,13 @@
 import React, { useRef, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import * as settings from './settings';
+import './../styles/App.css';
+// import axios from 'axios';
+import UploadFilesService from '../services/upload-file.service'
+import * as settings from '../util/settings';
 import { HiUpload } from 'react-icons/hi';
 import { TiDelete } from 'react-icons/ti';
 import { FaPlay } from 'react-icons/fa';
 import { FileDrop } from 'react-file-drop';
+import Spinner from 'react-bootstrap/Spinner'
 
 function Home () {
   const fileInputRef = useRef(null);
@@ -19,6 +21,9 @@ function Home () {
   const [file, setFile] = useState(null);
   const [filename, setFileName] = useState("unknown.mp4");
   const [response, setResponse] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [awaitingResponse, setAwaitingResponse] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const validateYTUrl = url => /^(?:https?:\/\/)?(w{3})?\.?youtube\.com/.test(url);
 
@@ -37,24 +42,49 @@ function Home () {
     setResponse(null);
   }
 
-  var processFile = () => {
+  var onUploadProgress = () => {
+
+  }
+
+  var uploadFile = () => {
+    setUploading(true);
     console.log(file);
-    
-    let form = new FormData();
-    form.append('file', file);
-
-    let headers = { 'Content-Type' : 'multipart/form-data'};
-    let url = settings.API_SERVER + '/api/predict/';
-    let method = 'post';
-    let config = { headers, method, url, data: form};
-
-    axios(config).then(
+    setAwaitingResponse(true);
+    UploadFilesService.upload(file, (event) => {
+      setProgress((100 * event.loaded) / event.total);
+      console.log(progress);
+    }).then(
       res => {
         console.log(res.data)
         setResponse(res);
+        setAwaitingResponse(false);
       }).catch(
-        error => {alert(error)}
+        error => {
+          alert(error);
+          setUploading(false);
+        }
+
       );
+
+    
+    // let form = new FormData();
+    // form.append('file', file);
+
+    // let headers = { 'Content-Type' : 'multipart/form-data'};
+    // let url = settings.API_SERVER + '/api/predict/';
+    // let method = 'post';
+    // let config = { headers, method, url, data: form};
+
+
+    // axios.post
+
+    // axios(config).then(
+    //   res => {
+    //     console.log(res.data)
+    //     setResponse(res);
+    //   }).catch(
+    //     error => {alert(error)}
+    //   );
   }
 
   var predictionBorderStyle = (confidence) => {
@@ -68,32 +98,31 @@ function Home () {
   }
 
   async function processURL () {
-    // const server = require('child_process').fork('./start_server.sh');
-    return;
-//    const url = document.getElementById('url').value;
-//    if (validateYTUrl(url)) {
-//      /* Process YouTube URL */
-//      const videoId = url.split("?")[1].match(/v=([^&]+)/)[1];
-//      return fetch(VIDEO_URL + videoId)
-//        .then(res => res.text())
-//        .then(res => {
-//          return qsToJson(res);
-//        });
-//    }
+  //   return;
+  //  const url = document.getElementById('url').value;
+  //  if (validateYTUrl(url)) {
+  //    /* Process YouTube URL */
+  //    const videoId = url.split("?")[1].match(/v=([^&]+)/)[1];
+  //    return fetch(VIDEO_URL + videoId)
+  //      .then(res => res.text())
+  //      .then(res => {
+  //        return qsToJson(res);
+  //      });
+  //  }
   }
-//
-//  function qsToJson(queryString) {
-//    let res = {};
-//    let params = queryString.split("&");
-//    let keyValuePair, key, value;
-//    for (let i in params) {
-//      keyValuePair = params[i].split("=");
-//      key = keyValuePair[0];
-//      value = keyValuePair[1];
-//      res[key] = decodeURIComponent(value);
-//    }
-//    return res;
-//  }
+
+ function qsToJson(queryString) {
+   let res = {};
+   let params = queryString.split("&");
+   let keyValuePair, key, value;
+   for (let i in params) {
+     keyValuePair = params[i].split("=");
+     key = keyValuePair[0];
+     value = keyValuePair[1];
+     res[key] = decodeURIComponent(value);
+   }
+   return res;
+ }
 
   var onFileInputChange = (event) => {
     const { files } = event.target;
@@ -106,13 +135,13 @@ function Home () {
     } else {
       return <div className="inlineButtons">
         <button className="btn" id="removeBtn" onClick={removeFile}><span className="filename">{filename}</span><TiDelete className="btnIcon"/></button>
-        <button className="btn" id="run" onClick={processFile}>RUN<FaPlay className="btnIcon"/></button>
+        <button className="btn" id="run" onClick={uploadFile}>RUN<FaPlay className="btnIcon"/></button>
       </div>
     }
   }
 
   function getURLInput() {
-    if (!haveFile) {
+    if (!haveFile && !uploading && !awaitingResponse) {
       return <div className="inlineButtons">
         <input className="input" id="url" type="url" placeholder="Paste a URL (ex: https://youtube.com/...)" onClick={console.log("click")}></input>
         <button className="btn" id="run" onClick={processURL}>RUN<FaPlay className="btnIcon"/></button>
@@ -122,11 +151,18 @@ function Home () {
   }
 
   function getContentArea() {
-    if (response == null) {
+    return getLoadingIndicator();
+    if (response == null && !uploading) {
       return getFileDropper();
+    } else if (uploading) {
+      return getLoadingIndicator();
     } else {
       return getPredictionView();
     }
+  }
+
+  function getLoadingIndicator() {
+    return <Spinner animation="border"/>
   }
 
   function getPredictionView() {
