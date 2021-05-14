d6a9ebe7a32a9e5a3caaa457ac2a5205b396611a
Chase Strickler <strickler.16@gmail.com>
2021-05-14 10:57:25 -0500

    Add ability to pass arguments to YouTube downloader module

--- backend/django_app/prediction/yt_download.py
+++ backend/django_app/prediction/yt_download.py
@@ -1,14 +1,53 @@
 # importing the module
 from pytube import YouTube
+import argparse
 
-def progress_func(stream, data_chunks, bytes_remaining):
-	print("Progress: {:.2f}%".format((FILE_SIZE - bytes_remaining) / FILE_SIZE * 100))
+class YouTubeDownloader():
+	file_size = -1
+	video = None
+	yt = None
 
-def complete_func(stream, file_path):
-	print("Completed, path: %s" % file_path)
+	def __init__(self, urls):
+		self._download(urls)
 
-YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo', on_progress_callback=progress_func, on_complete_callback=complete_func,)
-video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-FILE_SIZE = video.filesize
-video.download()
+	def progress_func(self, stream, data_chunks, bytes_remaining):
+		import sys
+		bar_len = 100
+		percent = (self.file_size - bytes_remaining) / self.file_size * 100
+		bar = '*' * int(round(percent)) + '.' * (bar_len - int(round(percent)))
+		sys.stdout.write("\rProgress [{}] {:.2f}%".format(bar, percent))
+		sys.stdout.flush()
+
+	def complete_func(self, stream, file_path):
+		print("\nComplete! Wrote file to: %s" % file_path)
+
+	def _download(self, urls):
+		for url in urls:
+			self.yt = YouTube(url, on_progress_callback=self.progress_func, on_complete_callback=self.complete_func,)
+			self.video = self.yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').asc().first()
+			self._print_metadata(self.yt, url)
+			self.file_size = self.video.filesize
+			self.video.download(output_path="videos/", filename='input')
+
+	def _print_metadata(self, yt, url):
+		print("\nDownloading video from URL: %s" % url)
+		print("---- Title: %s" % yt.title)
+		print("---- Author: %s" % yt.author)
+		print("---- Views: %d" % yt.views)
+		print("---- Channel URL: %s" % yt.channel_url)
+		print("---- Length: %d seconds" % yt.length)
+		print("---- Thumbnail URL: %s" % yt.thumbnail_url)
+		print("---- Description: %s" % yt.description)
+		print("\n")
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Download a video from YouTube given a URL')
+	parser.add_argument('urls', metavar='URLS', type=str, nargs='+', help='the url of the video to download')
+	args = parser.parse_args()
+	downloader = YouTubeDownloader(args.urls)
+
+
+
+if __name__=="__main__":
+	main()
